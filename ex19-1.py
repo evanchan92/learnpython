d05440cdb78a4cecd02647ea64136a6be6ec62a7
EvanCHEN <evanchan92@163.com>
2018-12-08 19:33:37 +0800

    s

---
+++ ex19-1.py
@@ -0,0 +1,16 @@
+# practice for functions!
+
+def meal(A,B,C):
+    print(f"There're {A} kinds of milk.")
+    print(f"And {B} kinds of chocolate.")
+    print(f"And {C} kinds of vegetables.")
+    print(f"That's {A + B + C} types together, we can open a supermarker!")
+
+meal(1,3,4)
+
+meal(2-3,12-8,2*14)
+
+x = 1234
+y = 12
+z = 4
+meal(x,y,z)
--- ex19.py
+++ ex19.py
@@ -1,4 +1,4 @@
-def cheese_and_crackers(cheese_count, boxes_of_crackers)：
+def cheese_and_crackers(cheese_count, boxes_of_crackers):
     print(f"You have {cheese_count} cheeses!")
     print(f"You have {boxes_of_crackers} boxes of crackers!")
     print("Man that's enough for a party!")
@@ -9,7 +9,7 @@ cheese_and_crackers(20, 30)
 
 print("Or, we can use variables from our script:")
 amount_of_cheese = 10
-amount_of_crackers = 50
+amount_of_crackers = 50A\
 
 cheese_and_crackers(amount_of_cheese, amount_of_crackers)
 
